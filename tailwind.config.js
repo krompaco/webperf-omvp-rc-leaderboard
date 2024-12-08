aac3df10d0ad2944372ee654f198d62b1ea951cc
Johan Kronberg <kronberg@gmail.com>
2024-12-08 16:31:51 +0100

    Fix path

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
 	content: [
 		'./docs/content-leaderboard/**/*.html',
+		'./src/Krompaco.RecordCollector.Web/**/*.html',
 		'./src/Krompaco.RecordCollector.Web/Components/**/*.razor',
 		'./src/Krompaco.RecordCollector.Web/Program.cs',
 	],
